7acda933464b18e0c2f2c12e83546ab9c09de533
SHRADDHA\Shraddha <shraddhasshetty123@gmail.com>
2023-12-23 10:30:10 +0530

    commit

--- 2.React-Django/myDjangoProject/level_two/second_app/views.py
+++ 2.React-Django/myDjangoProject/level_two/second_app/views.py
@@ -4,5 +4,6 @@ from second_app.models import Webpage,Topic,AccessRecord
 # Create your views here.
 
 def index(request):
+    web_page_list = AccessRecord.objects.order_by('date')
     insert_dict = {"insert_content_1": "Hi I AM A Django APP"}
     return render(request,'second_app/index.html',insert_dict)
