75b4c4c1461e21a1bbaf9b5507d5638d839e4506
SHRADDHA\Shraddha <shraddhasshetty123@gmail.com>
2023-12-23 20:47:46 +0530

    commit 1

--- 2.React-Django/myDjangoProject/level_two/second_app/admin.py
+++ 2.React-Django/myDjangoProject/level_two/second_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from second_app.models import Topic,Webpage,AccessRecord
+from second_app.models import Topic,Webpage,AccessRecord,Users
 
 admin.site.register(Topic)
 admin.site.register(Webpage)
 admin.site.register(AccessRecord)
+admin.site.register(Users)
 # Register your models here.
