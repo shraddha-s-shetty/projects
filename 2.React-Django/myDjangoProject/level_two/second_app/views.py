0915d75cd4a7397137aa87906c4e9cb2c33b32dd
SHRADDHA\Shraddha <shraddhasshetty123@gmail.com>
2023-12-23 20:11:06 +0530

    botcatcher

--- 2.React-Django/myDjangoProject/level_two/second_app/views.py
+++ 2.React-Django/myDjangoProject/level_two/second_app/views.py
@@ -16,4 +16,12 @@ def users(request):
 
 def form_name(request):
     form = forms.FormName()
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation success!")
+            print("Name: ",form.cleaned_data['name'])
+            print("Email: ",form.cleaned_data['email'])
+            print("Text: ",form.cleaned_data['text'])
     return render(request,'second_app/forms_page.html',{'form':form})
