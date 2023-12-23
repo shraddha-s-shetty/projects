14b78940f3b46f1ce17285645e0297ba4b1427b7
SHRADDHA\Shraddha <shraddhasshetty123@gmail.com>
2023-12-23 19:23:07 +0530

    django level three commit 1

--- 2.React-Django/myDjangoProject/level_two/second_app/views.py
+++ 2.React-Django/myDjangoProject/level_two/second_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from second_app.models import Webpage,Topic,AccessRecord,Users
-
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -13,3 +13,7 @@ def users(request):
     users_list = Users.objects.order_by('first_name')
     user_list = {'user_input':users_list}
     return render(request,'second_app/users.html',user_list)
+
+def form_name(request):
+    form = forms.FormName()
+    return render(request,'second_app/forms_page.html',{'form':form})
